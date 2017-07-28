23b46620e2b86c6d560a41dbabfcaaceac09cc36
Bruno Ukita <brunoukita@gmail.com>
2017-07-28 01:43:54 +0000

    refactor Currecy model test

--- test/models/currency_test.rb
+++ test/models/currency_test.rb
@@ -1,23 +1,32 @@
 require 'test_helper'
 
 class CurrencyTest < ActiveSupport::TestCase
+  def build_currency(params = {})
+    params = {
+      name: 'Bitcoin',
+      code: 'BTC'
+    }.merge(params)
+
+    Currency.new(params)
+  end
+
   test 'should insert with valid data' do
-    currency = Currency.new(name: 'Bitcoin', code: 'BTC')
+    currency = build_currency
     assert currency.save
   end
 
   test 'should validate presence of name' do
-    currency = Currency.new(name: nil, code: 'USD')
+    currency = build_currency(name: nil)
     assert_not currency.save
   end
 
   test 'should validate presence of code' do
-    currency = Currency.new(name: 'Dollar', code: nil)
+    currency = build_currency(code: nil)
     assert_not currency.save
   end
 
   test 'should validate uniqueness of code' do
-    currency = Currency.new(name: 'Dollar', code: 'USD')
+    currency = build_currency(code: 'USD')
     assert_not currency.save
   end
 end
